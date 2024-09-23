1254288efbbcc2a0309f6858e8cd0d406b5f718d
Dylan Runkel <dylan.runkel@gmail.com>
2024-09-22 20:54:40 -0700

    generate user model with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   respond_to :json
+
+  # before_action :authenticate_user!
 end
