cc833797243cccff39b01e348ff895c9f15b76e5
Dylan Runkel <dylan.runkel@gmail.com>
2024-10-16 21:39:07 -0700

    ride stats filter

--- app/controllers/api/v1/ride_stats_controller.rb
+++ app/controllers/api/v1/ride_stats_controller.rb
@@ -4,6 +4,22 @@ module Api
       def index
         start_date = 1.month.ago.beginning_of_day
         end_date = Time.current.end_of_day
+
+        if params[:range] == "year"
+          start_date = 1.year.ago.beginning_of_day
+          end_date = Time.current.end_of_day
+        end
+
+        if params[:range] == "last_7_days"
+          start_date = 7.days.ago.beginning_of_day
+          end_date = Time.current.end_of_day
+        end
+
+        if params[:range] == "last_30_days"
+          start_date = 30.days.ago.beginning_of_day
+          end_date = Time.current.end_of_day
+        end
+
         activities = current_user.strava_activities
                                  .where(type: "Ride")
                                  .where(start_date: start_date..end_date)
