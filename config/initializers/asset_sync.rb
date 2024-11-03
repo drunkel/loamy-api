5702516e416f7cbb8200a7bddde8387824b54b43
Dylan Runkel <dylan.runkel@gmail.com>
2024-11-03 08:47:07 -0800

    raw

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,16 +1,14 @@
-if defined?(AssetSync)
-  AssetSync.configure do |config|
-    config.fog_provider = "AWS"
-    config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
-    config.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
-    config.fog_directory = ENV["FOG_DIRECTORY"] # your S3 bucket name
-    config.fog_region = ENV["FOG_REGION"] # e.g., 'us-east-1'
-    config.aws_iam_roles = true
+AssetSync.configure do |config|
+  config.fog_provider = "AWS"
+  config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
+  config.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+  config.fog_directory = ENV["FOG_DIRECTORY"] # your S3 bucket name
+  config.fog_region = ENV["FOG_REGION"] # e.g., 'us-east-1'
+  config.aws_iam_roles = true
 
-    # Don't delete files from the store
-    config.existing_remote_files = "keep"
+  # Don't delete files from the store
+  config.existing_remote_files = "keep"
 
-    # Automatically replace files with their equivalent gzip version
-    config.gzip_compression = true
-  end
+  # Automatically replace files with their equivalent gzip version
+  config.gzip_compression = true
 end
